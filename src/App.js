1ee71ee428641bcc9dedfe46e5c1993e30634aaa
Teanna Barrett <teanna.barrett@bison.howard.edu>
2021-12-08 03:36:29 +0000

    Final product of basic app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import findAllSolutions from './solver.js';
+import Board from './Board.js';
+import GuessInput from './GuessInput.js';
+import FoundSolutions from './FoundSolutions.js';
+import SummaryResults from './SummaryResults.js';
+import ToggleGameState from './ToggleState.js';
+import logo from './logo.gif';
 import './App.css';
+import {GAME_STATE} from './GameState.js';
+import {RandomGrid} from './randomGen.js';
 
 function App() {
-  return (
+  const [allSolutions, setAllSolutions] = useState([]);  // solutions from solver
+  const [foundSolutions, setFoundSolutions] = useState([]);  // found by user
+  const [gameState, setGameState] = useState(GAME_STATE.BEFORE); // Just an enuerator or the three states see below
+  const [grid, setGrid] = useState([]);   // the grid
+  const [totalTime, setTotalTime] = useState(0);  // total time elapsed
+  const [size, setSize] = useState(3);  // selected grid size
+  
+  function correctAnswerFound(answer) {
+      console.log("New correct answer:" + answer);
+      setFoundSolutions([...foundSolutions, answer]);
+    }
+  
+  // useEffect will trigger when the array items in the second argument are
+  // updated so whenever grid is updated, we will recompute the solutions
+  useEffect(() => {
+    const wordList = require('./full-wordlist.json');
+    let tmpAllSolutions = findAllSolutions(grid, wordList.words);
+    setAllSolutions(tmpAllSolutions);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [grid]);
+
+  // This will run when gameState changes.
+  // When a new game is started, generate a new random grid and reset solutions
+  useEffect(() => {
+    if (gameState === GAME_STATE.IN_PROGRESS) {
+      setGrid(RandomGrid(size));
+      setFoundSolutions([]);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [gameState, size]);
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <img src={logo}  width="25%" height="25%" class="logo" alt="Plugged in Brain Cartoon" /> 
+        <h1>Mind Boggle-ing</h1>
+        <ToggleGameState gameState={gameState}
+                       setGameState={(state) => setGameState(state)} 
+                       setSize={(state) => setSize(state)}
+                       setTotalTime={(state) => setTotalTime(state)}/>
+
+      { gameState === GAME_STATE.IN_PROGRESS &&
+        <div>
+          <Board board={grid} />
+
+          <GuessInput allSolutions={allSolutions}
+                      foundSolutions={foundSolutions}
+                      correctAnswerCallback={(answer) => correctAnswerFound(answer)}/>
+          <FoundSolutions headerText="Solutions you've found" words={foundSolutions} />
+        </div>
+      }
+      { gameState === GAME_STATE.ENDED &&
+        <div>
+          <Board board={grid} />
+          <SummaryResults words={foundSolutions} totalTime={totalTime} />
+          <FoundSolutions headerText="Missed Words [wordsize > 3] " words={allSolutions}  />
+          
+        </div>
+      }
     </div>
   );
 }
---
+++ src/SummaryResults.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './FoundSolutions.css';
+
+function SummaryResults({ words, totalTime}) {
+
+  return (
+    <div className="Found-solutions-list">
+      <h2>SUMMARY</h2>
+     
+       <div>
+        <li key="12">Total Words Found: {words.length}</li>
+        
+       </div>
+      
+         <div>
+        <li key="15">Total Time: {totalTime} secs</li>
+        
+       </div>    
+    </div>
+  );
+}
+
+export default SummaryResults;
